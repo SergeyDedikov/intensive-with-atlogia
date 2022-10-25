ceaff0ca7c6bb8a0dcd150e23b9d255b80a9822d
SergeyDedikov <serded59@yandex.ru>
2022-10-25 15:35:44 +0500

    feat: actions for car buttons

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
 document.getElementById("main-action").onclick = function () {
   document.getElementById("cars").scrollIntoView({ behavior: "smooth" });
 };
+
+const carButtons = document.getElementsByClassName("car-button");
+const carInput = document.getElementById("car");
+
+for (let i = 0; i < carButtons.length; i++) {
+  let currentCarItem = carButtons[i].parentElement.parentElement;
+  let carTitle = currentCarItem.querySelector(".car-item-title").textContent;
+
+  carButtons[i].onclick = function () {
+    document.getElementById("price").scrollIntoView({ behavior: "smooth" });
+    carInput.value = carTitle;
+  };
+}
